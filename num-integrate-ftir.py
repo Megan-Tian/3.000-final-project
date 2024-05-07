37c0243c0f8a077383d2177b7fb2f027de6230c9
Megan-Tian <52009990+Megan-Tian@users.noreply.github.com>
2024-05-07 12:34:12 -0400

    elementary sensitivity analysis

--- num-integrate-ftir.py
+++ num-integrate-ftir.py
@@ -7,13 +7,12 @@ import pandas as pd
 def process_single_file(test, wavenumber_range):
     """
     Args:
-        test: path to .CSV
+        test: path object to .CSV
         wavenumber_range (tuple): (start, end)
     """
-    with test.open("r") as f:
-        lines = [x.split(",") for x in f.readlines()]
-        x = np.array([float(x[0].strip()) for x in lines])
-        y = np.array([float(x[1].strip()) for x in lines])
+    data = np.loadtxt(test, delimiter=",")
+    x = data[:, 0]
+    y = data[:, 1]
 
     total_auc = scipy.integrate.simpson(y, x)
 
@@ -25,6 +24,52 @@ def process_single_file(test, wavenumber_range):
     ratio = sucrose_auc / total_auc
 
     return total_auc, sucrose_auc, ratio
+
+
+def sensitivity_single_file(file, wavenumber_range, split_size):
+    '''
+    Returns DF with total auc and auc/ratios of "slices" within the wavenumber_range ratio.
+    Elementary sensitivity analysis - find which of the slices are most sensitive to sample changes
+
+    Args:
+        file: path object to .csv
+        wavenumber_range (tuple): (start, end)
+        split_size (int): size of each slice
+    '''
+    # assert (wavenumber_range[1] - wavenumber_range[0]) % split_size == 0
+    total_auc, sucrose_auc, ratio = process_single_file(file, wavenumber_range)
+    data = {'name': file.name, 'total_auc': total_auc}
+
+    for start_slice in range(wavenumber_range[0], wavenumber_range[1], split_size):
+        _, slice_auc, slice_ratio = process_single_file(file, (start_slice, start_slice + split_size))
+
+        # data.update({f"auc_{start_slice}_{start_slice + split_size}" :
+        #              slice_auc}) 
+        data.update({f"ratio_{start_slice}_{start_slice + split_size}" :
+                     slice_ratio}) 
+
+    return data, list(data.keys())
+
+def sensitivity_all_files(wavenumber_range, split_size):
+    # just to get headers for dataframe this is so bad ack
+    path = Path("data\\Ethiopia Genet 1.CSV")
+    _, headers = sensitivity_single_file(Path("data\\Ethiopia Genet 1.CSV"), wavenumber_range, split_size)
+    df = pd.DataFrame(columns=headers)
+
+    # real code now
+    data = Path("data")
+    
+    for file in data.iterdir():
+        if not file.name.endswith(".CSV"):
+            continue
+        
+        print(str(file))
+
+        results, _ = sensitivity_single_file(file, wavenumber_range, split_size)
+
+        df.loc[len(df)] = results
+
+    df.to_csv(f'sensitvity_{wavenumber_range[0]}_{wavenumber_range[1]}_split_{split_size}.csv', ',', header=True)
 
 
 def main():
@@ -43,6 +88,7 @@ def main():
         if not file.name.endswith(".CSV"):
             continue
 
+        print(str(file))
         total_auc, sucrose_auc, ratio = process_single_file(file, (750, 1500))
         _, sucrose_auc_2, ratio_2 = process_single_file(file, (3000, 3500))
 
@@ -57,5 +103,12 @@ def main():
 
     df.to_csv('results.csv', ',', header=True)
 
+
+
 if __name__ == "__main__":
-    main()
+    result, _ = sensitivity_single_file(Path("data\\Ethiopia Genet 1.CSV"), (750, 1500), split_size=10)
+
+    print(result)
+
+    sensitivity_all_files((750, 1500), split_size=10)
+    # main() 
